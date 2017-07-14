a5f42efd66fd2009ebb88fb9041329a18c4b2eee
Oscar Sánchez <oscarsanchez@MacBook-Pro-de-Oscar.local>
2017-07-14 11:02:16 -0500

    Adde ERP app

---
+++ ERP/models.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+from django.db import models
+from django.forms.models import model_to_dict
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
