dd800edb96c8d8f56cf1e186e687c806f6715898
agsneutron <ags_neutron@hotmail.com>
2017-07-23 14:46:19 -0500

    Merge remote-tracking branch 'remotes/origin/Memo-Branch'
    # Conflicts:
    #	ERP/admin.py

--- SalcedoERP/settings.py
+++ SalcedoERP/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'SalcedoERP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'users/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'users/templatess')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
