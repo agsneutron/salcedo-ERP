3fe2b2f3221769d8d6c72fe917395526032f704f
gsanchezn <nexus.gsan@gmail.com>
2017-07-23 21:29:03 -0500

    Pantalla de proyectos ajustada.

--- SalcedoERP/settings.py
+++ SalcedoERP/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'SalcedoERP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'users/templatess')],
+        'DIRS': [os.path.join(BASE_DIR, 'users/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
