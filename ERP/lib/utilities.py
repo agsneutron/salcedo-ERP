191add65ba16503f205437c85e5c3006d43c5c31
Oscar Sánchez <oscarsanchez@MacBook-Pro-de-Oscar.local>
2017-07-26 01:14:06 -0500

    Fix on utilities

--- ERP/lib/utilities.py
+++ ERP/lib/utilities.py
@@ -5,5 +5,5 @@ class Utilities():
 
     @staticmethod
     def query_set_to_json(query_set):
-        return mark_safe(json.dumps(map(lambda query_set: query_set.to_serializable_dict(), query_set.all()),
+        return mark_safe(json.dumps(map(lambda query_set: query_set.to_serializable_dict(), query_set),
                            ensure_ascii=False, indent=4, separators=(',', ': '), sort_keys=True,))
