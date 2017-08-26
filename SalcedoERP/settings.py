0dd9752a4cd845e2caa150dfa75a9beabe011e8c
Oscar Sánchez <oscar.sanjim@gmail.com>
2017-08-26 15:36:09 -0500

    Merge

--- SalcedoERP/settings.py
+++ SalcedoERP/settings.py
@@ -54,8 +54,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'SalcedoERP.urls'
 TEMPLATETAGS_DIRS = (
-    os.path.join(BASE_DIR, 'templatetags/'),
+    os.path.join(BASE_DIR, 'templatetags'),
 )
+
+print "Tags:"
+print os.path.join(BASE_DIR, 'templatetags')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
