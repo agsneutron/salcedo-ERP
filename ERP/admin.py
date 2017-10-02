3911cd6ec40f62c2f7bf1c0954b37d267dcbd754
Oscar Sánchez <oscar.sanjim@gmail.com>
2017-10-02 18:51:28 -0500

    Added context variables  to the edit view of a project, to control the settings

--- ERP/admin.py
+++ ERP/admin.py
@@ -840,6 +840,44 @@ class ProjectModelAdmin(admin.ModelAdmin):
 
         return ModelForm
 
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+
+        # Setting the extra variable to the set context or none instead.
+        extra = extra_context or {}
+
+        project_obj = Project.objects.get(pk=object_id)
+
+        # Getting the settings for the current project, to know which card details to show.
+        project_sections = ProjectSections.objects.filter(
+            Q(project_id=project_obj.id) & Q(section__parent_section=None))
+        sections_result = []
+        for section in project_sections:
+            section_json = {
+                "section_name": section.section.sectionName,
+                "section_id": section.section.id,
+                "total_inner_sections": 0,
+                "inner_sections": []
+            }
+            i = 0
+            inner_sections = ProjectSections.objects.filter(
+                Q(project_id=project_obj.id) & Q(section__parent_section=section.section) & Q(status=1))
+            for inner_section in inner_sections:
+                inner_json = {
+                    "inner_section_name": inner_section.section.sectionName,
+                    "inner_section_id": inner_section.section.id,
+                    "inner_section_status": inner_section.status,
+                }
+                i += 1
+                section_json["inner_sections"].append(inner_json)
+            section_json["total_inner_sections"] = i
+            sections_result.append(section_json)
+
+        extra['sections_result'] = sections_result
+
+
+        return super(ProjectModelAdmin, self).change_view(request, object_id,
+                                                     form_url, extra_context=extra)
+
 
 @admin.register(Estimate)
 class EstimateAdmin(admin.ModelAdmin):
