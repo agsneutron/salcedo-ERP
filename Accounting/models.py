8455c737bc522d3ce5d70a56a403c0a3b1129e8a
ariaocho <ariaocho@gmail.com>
2017-12-07 23:28:23 -0600

    GroupingCode for accounts model

--- Accounting/models.py
+++ Accounting/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+# Model for grouping code for accounts
+class GroupingCode(models.Model):
+    level = models.IntegerField(verbose_name="Nivel", )
+    grouping_code = models.DecimalField(verbose_name="Código Agrupador", max_digits=20, decimal_places=2,)
+    account_name = models.CharField(verbose_name="Nombre de la Cuenta y/o subcuenta",)
