8a72299c5d1580ae50ee692320e89e72931cde63
agsneutron <ags_neutron@hotmail.com>
2017-12-12 16:24:51 -0600

    Merge remote-tracking branch 'remotes/origin/Ari-Branch'
    # Conflicts:
    #	Accounting/models.py

--- Accounting/admin.py
+++ Accounting/admin.py
@@ -5,26 +5,13 @@ from django.contrib import admin
 
 # Register your models here.
 from Accounting.models import *
+from Accounting.forms import *
 
 
 @admin.register(AccountingPolicy)
 class AccountingPolicyAdmin(admin.ModelAdmin):
-    form = AccountingPolicy
+    form = AccountingPolicyForm
 
-
-    fieldsets = (
-        ("Datos de Empleado", {
-            'fields': ('fiscal_period', 'type_policy', 'folio', 'registry_date', 'description')
-        }),
-        ("Datos Personales", {
-            'fields': (
-                'name', 'first_last_name', 'second_last_name', 'birthdate', 'birthplace', 'gender', 'marital_status',
-                'curp', 'rfc', 'tax_regime', 'social_security_number', 'social_security_type', 'blood_type','street', 'outdoor_number', 'indoor_number', 'colony',
-                'country', 'state', 'town', 'zip_code', 'phone_number', 'cellphone_number', 'office_number',
-                'extension_number', 'personal_email', 'work_email', 'driving_license_number',
-                'driving_license_expiry_date')
-        }),
-    )
 
 
 admin.site.register(GroupingCode)
