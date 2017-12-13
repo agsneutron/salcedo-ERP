73bfc32cf4904107da5b3414d02231d9c812cbab
JoseCarranzaCortez <pepee.carranza@gmail.com>
2017-12-12 22:20:07 -0600

    Finished account search engine

--- Accounting/search_engines/account_engine.py
+++ Accounting/search_engines/account_engine.py
@@ -3,18 +3,26 @@ from django.db.models import Q
 from Accounting.models import Account
 
 
-class AccountSearchEngine():
+class AccountSearchEngine(object):
+    """
+        Search engines for the Account model
+    """
+
     def __init__(self, number, name, subsidiary_account_array, nature_account_array, grouping_code_array, level,
-                 item_array):
+                 item):
         self.number = number
         self.name = name
         self.subsidiary_account_array = subsidiary_account_array
         self.nature_account_array = nature_account_array
         self.grouping_code_array = grouping_code_array
         self.level = level
-        self.item_array = item_array
+        self.item = item
 
     def search(self):
+        """
+        Searches for accounts according to the parameters set in constructor
+        :return: Queryset with the results
+        """
         q = Q()
 
         if self.number is not None:
@@ -34,5 +42,8 @@ class AccountSearchEngine():
 
         if self.level is not None:
             q = q & Q(level__icontains=str(self.level))
+
+        if self.item is not None:
+            q = q & Q(item__icontains=str(self.item))
 
         return Account.objects.filter(q)
