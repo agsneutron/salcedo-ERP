5861b4fa70e740c6f72a6ce7025671614c525b74
ariaocho <ariaocho@gmail.com>
2017-12-13 00:20:00 -0600

    Merge remote-tracking branch 'remotes/origin/Memo-branch'
    Conflicts:
    	Accounting/admin.py
    	HumanResources/templates/HumanResources/employee-detail.html

--- Accounting/urls.py
+++ Accounting/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from Accounting import api
+from Accounting import views
 
 app_name = 'DataUpload'
 
@@ -8,6 +9,7 @@ urlpatterns = [
 
     url(r'^search_policies', api.SearchPolicies.as_view(), name='policy-engine'),
     url(r'^search_accounts', api.SearchAccounts.as_view(), name='account-engine'),
-    url(r'^search_providers', api.SearchProviders.as_view(), name='provider-engine')
+    url(r'^search_providers', api.SearchProviders.as_view(), name='provider-engine'),
+    url(r'^searchaccount', views.SearchAccount, name='searchaccount')
 
 ]
--- Accounting/views.py
+++ Accounting/views.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.template import RequestContext,loader
+from Accounting.models import *
+from django.http import HttpResponse
 
 from django.shortcuts import render
 
 # Create your views here.
+
+# For Search Account filter objects view
+def SearchAccount(request):
+    template = loader.get_template('Accounting/search_account.html')
+    context = RequestContext(request, {
+        'account': Account.objects.all(),
+
+    })
+    return HttpResponse(template.render(context))
