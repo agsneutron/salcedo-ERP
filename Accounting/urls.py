d3243a0e80838b841270eccccd791fa7b6e0b787
ariaocho <ariaocho@gmail.com>
2017-12-13 20:19:45 -0600

    estructura para accountingpolicy

--- Accounting/urls.py
+++ Accounting/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 
     url(r'^search_policies', api.SearchPolicies.as_view(), name='policy-engine'),
     url(r'^search_accounts', api.SearchAccounts.as_view(), name='account-engine'),
-    url(r'^search_commercial_allies', api.SearchProviders.as_view(), name='commercial-allies-engine')
-    url(r'^searchaccount', views.SearchAccount, name='searchaccount')
+    url(r'^search_commercial_allies', api.SearchProviders.as_view(), name='commercial-allies-engine'),
+    url(r'^searchaccount', views.SearchAccount, name='searchaccount'),
+    url(r'^searchcomercialallie', views.SearchComercialAllie, name='searchcomercialallie')
 ]
