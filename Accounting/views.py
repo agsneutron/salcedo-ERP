ec02d68a71bdea2bf564a99fa0ba6f9a136f2fdc
g.sanchez <nexus.gsan@gmail.com>
2017-12-18 02:00:12 -0600

    vista detalle

--- Accounting/views.py
+++ Accounting/views.py
@@ -7,6 +7,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def PolicieDetail(request):
+    template = loader.get_template('Accounting/policie-detail.html')
+    return HttpResponse(template.render(request))
 
 # For Search Account filter objects view
 def SearchAccount(request):
