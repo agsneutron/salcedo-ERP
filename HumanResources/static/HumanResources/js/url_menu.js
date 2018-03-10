7b657b66a21dbb5d4a1e1c176ae94de8d628b5fb
g.sanchez <nexus.gsan@gmail.com>
2018-03-10 02:36:56 -0600

    eventos active en menu lateral de contratacion

---
+++ HumanResources/static/HumanResources/js/url_menu.js
@@ -0,0 +1,131 @@
+$(function () {
+    if (/tag/.test(window.location.href)) {
+        $('#Conte2>a').addClass('active');
+        $('#Conte2>a').removeClass('collapsed');
+    }
+
+});
+$(function () {
+    if (/tag\/+add/.test(window.location.href)) {
+        $('#Conte2>a').addClass('active');
+        $('#Conte2>a').removeClass('collapsed');
+    }
+
+});
+
+$(function () {
+    if (/test/.test(window.location.href)) {
+        $('#Conte1>a').addClass('active');
+        $('#Conte1>a').removeClass('collapsed');
+    }
+
+});
+$(function () {
+    if (/test\/+add/.test(window.location.href)) {
+        $('#Conte1>a').addClass('active');
+        $('#Conte1>a').removeClass('collapsed');
+    }
+
+});
+
+$(function () {
+    if (/documenttype/.test(window.location.href)) {
+        $('#Conte0>a').addClass('active');
+        $('#Conte0>a').removeClass('collapsed');
+    }
+
+});
+$(function () {
+    if (/documenttype\/+add/.test(window.location.href)) {
+        $('#Conte0>a').addClass('active');
+        $('#Conte0>a').removeClass('collapsed');
+    }
+
+});
+
+$(function () {
+    if (/direction/.test(window.location.href)) {
+        $('#Conte3>a').addClass('active');
+        $('#Conte3>a').removeClass('collapsed');
+    }
+
+});
+$(function () {
+    if (/direction\/+add/.test(window.location.href)) {
+        $('#Conte3>a').addClass('active');
+        $('#Conte3>a').removeClass('collapsed');
+    }
+
+});
+
+$(function () {
+    if (/subdirection/.test(window.location.href)) {
+        $('#Conte4>a').addClass('active');
+        $('#Conte4>a').removeClass('collapsed');
+
+         $('#Conte3>a').removeClass('active');
+ $('#Conte3>a').addClass('collapsed withripple');
+    }
+
+});
+$(function () {
+    if (/subdirection\/+add/.test(window.location.href)) {
+        $('#Conte4>a').addClass('active');
+        $('#Conte4>a').removeClass('collapsed');
+    }
+
+});
+
+$(function () {
+    if (/area/.test(window.location.href)) {
+        $('#Conte5>a').addClass('active');
+        $('#Conte5>a').removeClass('collapsed');
+    }
+
+});
+$(function () {
+    if (/area\/+add/.test(window.location.href)) {
+        $('#Conte5>a').addClass('active');
+        $('#Conte5>a').removeClass('collapsed');
+    }
+
+});
+
+$(function () {
+    if (/department/.test(window.location.href)) {
+        $('#Conte6>a').addClass('active');
+        $('#Conte6>a').removeClass('collapsed');
+    }
+
+});
+$(function () {
+    if (/department\/+add/.test(window.location.href)) {
+        $('#Conte6>a').addClass('active');
+        $('#Conte6>a').removeClass('collapsed');
+    }
+
+});
+
+$(function () {
+    if (/employeerequisition/.test(window.location.href)) {
+        $('#Conte7>a').addClass('active');
+        $('#Conte7>a').removeClass('collapsed');
+    }
+
+});
+$(function () {
+    if (/employeerequisition\/+add/.test(window.location.href)) {
+        $('#Conte7>a').addClass('active');
+        $('#Conte7>a').removeClass('collapsed');
+    }
+
+});
+/*
+$(function () {
+    if (/\?+type=1/.test(window.location.href)) {
+        $('#e4>a').addClass('active');
+        $('#e4>a').removeClass('collapsed');
+    }
+});
+*/
+
--- static/Accounting/js/ajax_searchcommercialally.js
+++ static/Accounting/js/ajax_searchcommercialally.js
@@ -8,15 +8,27 @@ var $j = jQuery.noConflict();
 
 $j(document).on('ready', main);
 
-function main(){
-     $j('#searchaccount').on('click', search);
+function main() {
+    $j('#searchaccount').on('click', search);
+    $j('#excelexport').on('click', export_excel);
+    search();
 }
 
- //http://127.0.0.1:8000/accounting/search_commercial_allies?
- //   name=nombre&rfc=12345678&email=test@test.com&phone_number=123456789&accounting_account_number=5&
- //   bank_account=123456789&register_date_lower=12/12/2017&register_date_upper=12/14/2017&type=PROVIDER
+//http://127.0.0.1:8000/accounting/search_commercial_allies?
+//   name=nombre&rfc=12345678&email=test@test.com&phone_number=123456789&accounting_account_number=5&
+//   bank_account=123456789&register_date_lower=12/12/2017&register_date_upper=12/14/2017&type=PROVIDER
 
 function search() {
+    perform_search(false);
+}
+
+function export_excel() {
+    perform_search(true);
+}
+
+
+function perform_search(excel_export) {
+
     var name = $j("#name").val();
     var rfc = $j("#rfc").val();
     var email = $j("#email").val();
@@ -27,46 +39,58 @@ function search() {
     var register_date_upper = $j("#register_date_upper").val();
     var type = $j("#type").val();
     var url = "/accounting/search_commercial_allies?";
+    var services = $j("#services").val();
 
-    if (name.toString()!="") {
+    if (name.toString() != "") {
         url = url + "name=" + name.toString() + "&";
     }
-    if (rfc.toString()!="") {
-        url=url+"rfc="+rfc.toString() + "&";
+    if (rfc.toString() != "") {
+        url = url + "rfc=" + rfc.toString() + "&";
     }
-    if (email.toString()!="") {
-        url=url+"email="+email.toString() + "&";
+    if (email.toString() != "") {
+        url = url + "email=" + email.toString() + "&";
     }
-    if (phone_number.toString()!="") {
-        url=url+"phone_number="+phone_number.toString() + "&";
+    if (phone_number.toString() != "") {
+        url = url + "phone_number=" + phone_number.toString() + "&";
     }
-    if (accounting_account_number.toString()!="") {
-        url=url+"accounting_account_number="+accounting_account_number.toString() + "&";
+    if (accounting_account_number.toString() != "") {
+        url = url + "accounting_account_number=" + accounting_account_number.toString() + "&";
     }
-    if (bank_account.toString()!="") {
-        url=url+"bank_account="+bank_account.toString() + "&";
+    if (bank_account.toString() != "") {
+        url = url + "bank_account=" + bank_account.toString() + "&";
     }
-    if (register_date_lower.toString()!="") {
-        url=url+"register_date_lower="+register_date_lower.toString() + "&";
+    if (register_date_lower.toString() != "") {
+        url = url + "register_date_lower=" + register_date_lower.toString() + "&";
     }
-    if (register_date_upper.toString()!="") {
-        url=url+"register_date_upper="+register_date_upper.toString() + "&";
+    if (register_date_upper.toString() != "") {
+        url = url + "register_date_upper=" + register_date_upper.toString() + "&";
     }
-    if (type.toString()!="") {
+
+    if (services.toString() != "") {
+        url = url + "services=" + services.toString() + "&";
+    }
+
+    if (excel_export) {
+        url = url + "export_excel=1" + "&";
+    }
+
+
+    if (type.toString() != "") {
         url = url + "type=" + type.toString();
-        searchengine(url,type);
+
+        if (excel_export) {
+            window.location.replace(url)
+        } else {
+            searchengine(url, type)
+        }
     }
-    else{
+    else {
         console.log("No TYPE");
     }
-    //alert(url);
-
 }
 
 
-
-
-function searchengine(url,type) {
+function searchengine(url, type) {
     // Setup CSRF tokens and all that good stuff so we don't get hacked
     $.ajaxSetup(
         {
@@ -83,14 +107,14 @@ function searchengine(url,type) {
     /*  $.get('/obras/register_by_token', function(ans) {
      var ajaxData = { access_token: ans.access_token};
      //alert(ans.access_token);*/
-    var message="";
+    var message = "";
 
     $.ajax({
         url: url,
         type: 'get',
         success: function (data) {
             //console.log(data);
-            displayResults(data,type);
+            displayResults(data, type);
 
         },
         error: function (data) {
@@ -108,58 +132,58 @@ function searchengine(url,type) {
     /* });*/
 }
 
-function displayResults(data,type){
-    var sHtml="";
-    var sTable="";
+function displayResults(data, type) {
+    var sHtml = "";
+    var sTable = "";
 
     $j('#divTable').html("<div></div>");
-    sHtml ='<table class="table-filtros table" cellspacing="0" width="100%" id="tablaResultados">'
-            + ' <colgroup>'
-                +' <col width="25%">'
-                +' <col width="10%">'
-                +' <col width="10%">'
-                +' <col width="15%">'
-                +' <col width="10%">'
-                //+' <col width="5%">'
-                +' <col width="5%">'
-                +' <col width="5%">'
-                +' <col width="5%">'
-                +' </colgroup>';
+    sHtml = '<table class="table-filtros table" cellspacing="0" width="100%" id="tablaResultados">'
+        + ' <colgroup>'
+        + ' <col width="">'
+        + ' <col width="">'
+        + ' <col width="">'
+        + ' <col width="">'
+        + ' <col width="">'
+        //+' <col width="5%">'
+        + ' <col width="5%">'
+        + ' <col width="5%">'
+        + ' <col width="5%">'
+        + ' </colgroup>';
 
-    sTable= '<thead>'
-                        +'<tr>'
-                            +'<th>Nombre</th>'
-                            +'<th>RFC</th>'
-                            +'<th>E-mail</th>'
-                            +'<th>Cuenta Contable</th>'
-                            +'<th>Banco</th>'
-                           // +'<th>Registro</th>'
-                            +'<th>Ver</th>'
-                            +'<th>Editar</th>'
-                            +'<th>Eliminar</th>'
+    sTable = '<thead>'
+        + '<tr>'
+        + '<th>Nombre</th>'
+        + '<th>RFC</th>'
+        + '<th>E-mail</th>'
+        + '<th>Cuenta Contable</th>'
+        + '<th>Banco</th>'
+        // +'<th>Registro</th>'
+        + '<th class="no-sorting">Ver</th>'
+        + '<th class="no-sorting">Editar</th>'
+        + '<th class="no-sorting">Eliminar</th>'
 
-                        +'</tr>'
-                    +'</thead>'
-                    +'<tbody>';
+        + '</tr>'
+        + '</thead>'
+        + '<tbody>';
 
     for (var i = 0; i < data.length; i++) {
         var type_id = ""
-            if (type == "PROVIDER"){
-                type_id=0
-            }
-            else if (type == "CREDITOR"){
-                type_id = 1
-            }
-            else if (type == "THIRD_PARTY"){
-                type_id = 2
-            }
+        if (type == "PROVIDER") {
+            type_id = 0
+        }
+        else if (type == "CREDITOR") {
+            type_id = 1
+        }
+        else if (type == "THIRD_PARTY") {
+            type_id = 2
+        }
 
-            sTable += '<tr>'
-            + '<td class="result1 selectable">'+ data[i].name + '</td>'
-            + '<td class="result1 selectable">'+ data[i].rfc + '</td>'
-            + '<td class="result1 selectable">'+ data[i].email + '</td>'
-            + '<td class="result1 selectable">'+ data[i].accounting_account + '</td>'
-            + '<td class="result1 selectable">'+ data[i].bank_account_name + '</td>'
+        sTable += '<tr>'
+            + '<td class="result1 selectable">' + data[i].name + '</td>'
+            + '<td class="result1 selectable">' + data[i].rfc + '</td>'
+            + '<td class="result1 selectable">' + data[i].email + '</td>'
+            + '<td class="result1 selectable">' + data[i].accounting_account + '</td>'
+            + '<td class="result1 selectable">' + data[i].bank_account_name + '</td>'
             //+ '<td class="result1 selectable">'+ data[i].register_date + '</td>'
             + '<td class="text-center" style="width: 3%"><a '
             + 'href="/admin/Accounting/commercialally/' + data[i].id + '" '
@@ -176,53 +200,62 @@ function displayResults(data,type){
             + '</tr>'
     }
 
-    sTable +='</tbody>'
-          +'</table>';
+    sTable += '</tbody>'
+        + '</table>';
 
-    sHtml +=sTable;
+    sHtml += sTable;
 
-    sScript='<script id="js" type="text/javascript"  class="init">'
-	        +'$("#tablaResultados").DataTable( {'
-            + 'destroy: true,';
+    sScript = '<script id="js" type="text/javascript"  class="init">'
+        + '$("#tablaResultados").DataTable( {'
 
-    sTable ='columnDefs: ['
-             +'       {'
-             +'           className: "mdl-data-table__cell--non-numeric"'
-             +'       }'
-             +'   ],'
+    sTable = 'columnDefs: ['
+        + '       {'
+        + '           className: "mdl-data-table__cell--non-numeric"'
+        + '       }'
+        + '   ],'
 
-             +'   responsive: true,'
-             +'   "bInfo": false,';
+        + '   responsive: true,'
+        + '   "bInfo": false,';
 
     sScript += sTable;
-    sScript +=' "pageLength": 6,';
+    sScript += ' "pageLength": 6,';
 
     sTable = '   "bLengthChange": false,'
-             +'   "language": {'
-             +'       "sProcessing": "Procesando...",'
-             +'       "sLengthMenu": "Mostrar _MENU_ registros",'
-             +'       "sZeroRecords": "No se encontraron resultados",'
-             +'       "sEmptyTable": "La consulta no generó resultados a mostrar",'
-             +'       "sInfo": "",'
-             +'       "sInfoEmpty": "",'
-             +'       "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",'
-             +'       "sInfoPostFix": "",'
-             +'       "sSearch": "",'
-             +'      "sUrl": "",'
-             +'       "sInfoThousands": ",",'
-             +'       "sLoadingRecords": "Cargando...",'
-             +'       "oPaginate": {'
-             +'           "sFirst": "Primero",'
-             +'           "sLast": "Último",'
-             +'          "sNext": ">",'
-             +'           "sPrevious": "<"'
-          +'}'
-          +'},'
-	      +'} );'
-          +'</script>';
+        + '   "language": {'
+        + '       "sProcessing": "Procesando...",'
+        + '       "sLengthMenu": "Mostrar _MENU_ registros",'
+       /* + '       "sZeroRecords": "No se encontraron resultados",'*/
+    +' "sEmptyTable": function(){\n' +
+        '\n' +
+        ' \t $(\'.dataTables_paginate\').addClass(\'hide\');\n' +
+        ' \t $(\'.sorting\').addClass(\'no-sorting\');\n' +
+        '\n' +
+        ' \treturn "La consulta no generó resultados a mostrar"; }\n' +
+        '\t\t\t,'
+        + '       "sInfo": "",'
+        + '       "sInfoEmpty": "",'
+        + '       "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",'
+        + '       "sInfoPostFix": "",'
+        + '       "sSearch": "",'
+        + '      "sUrl": "",'
+        + '       "sInfoThousands": ",",'
+        + '       "sLoadingRecords": "Cargando...",'
+        + '       "oPaginate": {'
+        + '           "sFirst": "Primero",'
+        + '           "sLast": "Último",'
+        + '          "sNext": ">",'
+        + '           "sPrevious": "<"'
+        + '}'
+        + '},'
+
+        + '} );'
+        + ''
+
+
+        + '</script>';
 
     sScript += sTable;
 
-    $j('#divTable').html(sHtml+sScript);
+    $j('#divTable').html(sHtml + sScript);
 
 }
