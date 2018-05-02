ae27acbe4aa7f75080f24aca0d196a706fb07e6c
ariaocho <ariaocho@gmail.com>
2018-05-01 20:27:43 -0500

    add change password module

--- SalcedoERP/urls.py
+++ SalcedoERP/urls.py
@@ -60,4 +60,9 @@ urlpatterns = [
 
     url(r'^tinymce/', include('tinymce.urls')),
 
+    url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
+    url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
