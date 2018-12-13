e548247785b188eb6f0c16f3afa32f31e03e1e12
victor <alfonsobeltran135@gmail.com>
2018-12-13 14:45:31 -0600

    Merge remote-tracking branch 'remotes/origin/javier-Branch' into victor-Branch
    # Conflicts:
    #	HumanResources/static/HumanResources/js/ajax_loan.js

--- HumanResources/static/HumanResources/js/ajax_loan.js
+++ HumanResources/static/HumanResources/js/ajax_loan.js
@@ -2,20 +2,16 @@ $(document).ready(function () {
     var activado = 0;
     $('tr.add-row td a').click(function (){
 
+        //location.href="#";
+        //alert("activado pre if" + activado);
         if (activado==0) {
-
+             // alert("activado post if" + activado);
             activado = 1;
+            // alert("activado a 1" + activado);
             limite = $('#id_payment_plan').val();
             amount = $('#id_amount').val();
 
-        if (amount==0) {
-
-                window.alert('Falta llenar los campos: Empleado, Cantidad o Fecha');
-                  //alert("Vriable " + amount + " amount");
-                location.reload();
-
-        }else{
-
+            //alert(limite);
             if (limite==1){
                 limite = 12;
             }
@@ -23,7 +19,7 @@ $(document).ready(function () {
                 limite=14;
             }
 
-            for (var i = 0; i < limite; i++) {
+            for (var i = 0; i < limite-1; i++) {
                // alert("i" + i);
                 $link = $('tr.add-row td a');
                 $link[0].click()
@@ -47,6 +43,9 @@ $(document).ready(function () {
                 }
 
             }
+
+
+
         }
     });
 
