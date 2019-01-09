b41d7969b7a8e80ea579632e3a0c3a75f64452c3
agsneutron <ags_neutron@hotmail.com>
2019-01-09 01:17:44 -0600

    paysheet report

---
+++ Accounting/reports/paysheet_report.py
@@ -0,0 +1,268 @@
+# coding=utf-8
+from django.db.models.aggregates import Count, Sum
+
+import StringIO
+from rexec import FileWrapper
+
+from django.http.response import StreamingHttpResponse
+from xlsxwriter.workbook import Workbook
+
+from Accounting.models import FiscalPeriod
+
+
+class PaySheetReport():
+
+    @staticmethod
+    def generate_report(general_data, paysheet_details_set):
+        """
+        Entry point to generate the trial report.
+
+        :param general_data: General Data for the report, such as Fiscal Period, Year, etc.
+        :param policies_details_set: Set of policy details to be processed.
+        :return: HttpResponse containing the downloadable file.
+        """
+
+        output = StringIO.StringIO()
+        workbook = Workbook(output)
+        worksheet = workbook.add_worksheet('Nómina')
+
+
+        if len(paysheet_details_set) == 0:
+            # Widen the first column to make the text clearer.
+            #worksheet.write('A1', 'No se encontraron datos pertenecientes al periodo.')
+            pass
+
+
+        else:
+            PaySheetReport.add_headers(workbook, worksheet, general_data)
+            PaySheetReport.add_paysheet_details(workbook, worksheet, paysheet_details_set)
+
+
+
+        workbook.close()
+        response = StreamingHttpResponse(FileWrapper(output),
+                                         content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+        response['Content-Disposition'] = 'attachment; filename=Balanza_de_Comprobación.xlsx'
+        response['Content-Length'] = output.tell()
+
+        output.seek(0)
+
+        return response
+
+
+    @staticmethod
+    def add_headers(workbook, worksheet, general_data):
+        """
+        Adds the necessary headers to the worksheet.
+
+        :param workbook: Workbook Object.
+        :param worksheet: Worksheet Object.
+        :param general_data: Data to set info in the headers section.
+        """
+
+        formats = PaySheetReport.get_formats(workbook)
+
+        # Formats
+        blue_header_format_info = formats['blue_header_format_info']
+        white_centered_bold_header = formats['white_centered_bold_header']
+        white_bold_header = formats['white_bold_header']
+
+
+        worksheet.set_column('A:F', 20)
+
+
+        # Row 1.
+        if general_data['title'] is None or general_data['title'] == "":
+            title = "Balance de Comprobación de Sumas y Saldos"
+        else:
+            title = general_data['title']
+
+        worksheet.merge_range('A1:F1', title, white_centered_bold_header)
+
+        # Row 2.
+        worksheet.merge_range('A2:C2', "Salcedo Construcción y Supervisión", white_bold_header)
+        # Line Commented until definition of CUIT.
+        #worksheet.merge_range('D2:F2', "CUIT", white_bold_header)
+
+        # Row 3.
+        month_number = int(general_data['month'])
+        year_number = int(general_data['year'])
+
+        worksheet.merge_range('A3:C3', FiscalPeriod.get_month_name_from_number(month_number) + " " + str(year_number), white_bold_header)
+
+
+        # Table headers.
+        worksheet.merge_range('A5:A6', "N. Orden", blue_header_format_info)
+        worksheet.merge_range('B5:B6', "Cuenta", blue_header_format_info)
+
+        worksheet.merge_range('C5:D5', "Sumas", blue_header_format_info)
+        worksheet.write('C6', "Debe", blue_header_format_info)
+        worksheet.write('D6', "Haber", blue_header_format_info)
+
+        worksheet.merge_range('E5:F5', "Sumas", blue_header_format_info)
+        worksheet.write('E6', "Deudor", blue_header_format_info)
+        worksheet.write('F6', "Acreedor", blue_header_format_info)
+
+
+    @staticmethod
+    def get_formats(workbook):
+        formats = {}
+
+        # Format with blue background and white letters.
+        blue_header_format_info = {
+            'bold': 1,
+            'border': 1,
+            'align': 'center',
+            'valign': 'vcenter',
+            'fg_color': '#3E84CF',
+            'border_color': '#000000',
+            'font_color': '#FFFFFF'
+        }
+        formats['blue_header_format_info'] = workbook.add_format(blue_header_format_info)
+
+        # Format with white background and bold centered text.
+        white_centered_bold_header_info = {
+            'align': 'center',
+            'valign': 'vcenter',
+            'bold': 1
+        }
+        formats['white_centered_bold_header'] = workbook.add_format(white_centered_bold_header_info)
+
+        # Format with white background and bold text aligned to the left.
+        white_bold_header_info = {
+            'align': 'left',
+            'valign': 'vcenter',
+            'bold': 1
+        }
+        formats['white_bold_header'] = workbook.add_format(white_bold_header_info)
+
+        # Format with white background and bold text aligned to the left with black border.
+        white_bold_record_info = {
+            'align': 'left',
+            'valign': 'vcenter',
+            'bold': 1,
+            'border': 1,
+            'border_color':'#000000'
+        }
+        formats['white_bold_record'] = workbook.add_format(white_bold_record_info)
+
+        # Format with white background and bold text aligned to the right with black border for currency.
+        white_bold_right_currency_info = {
+            'align': 'right',
+            'valign': 'vcenter',
+            'border': 1,
+            'border_color': '#000000',
+            'num_format': '$#,#00.00',
+            'bold':1
+        }
+        formats['white_bold_right_currency'] = workbook.add_format(white_bold_right_currency_info)
+
+        # Format with gray background and bold text aligned to the right with black border for currency.
+        gray_bold_right_currency_info = {
+            'align': 'right',
+            'valign': 'vcenter',
+            'border': 1,
+            'border_color': '#000000',
+            'num_format': '$#,#00.00',
+            'bold': 1,
+            'fg_color':'#E8E8E8'
+        }
+        formats['gray_bold_right_currency'] = workbook.add_format(gray_bold_right_currency_info)
+
+        # Format with yellow background and bold text aligned to the right.
+        yellow_total_right_info = {
+            'align': 'right',
+            'valign': 'vcenter',
+            'border': 1,
+            'border_color': '#000000',
+            'bold': 1,
+            'fg_color': '#FDB709'
+        }
+        formats['yellow_total_right'] = workbook.add_format(yellow_total_right_info)
+
+
+        return formats
+
+
+    @staticmethod
+    def add_paysheet_details(workbook, worksheet, paysheet_detail_set):
+        """
+        Takes a set of policy details and processes it to define the amount of debit and credit related to an
+         account.
+
+        :param workbook: Workbook Object.
+        :param worksheet: Worksheet Object.
+        :param policies_set: Set of policies that were found for the given fiscal period and account ranges.
+
+        """
+        START_ROW = 6
+
+        COUNTER_COL = 0
+        ACCOUNT_COL = 1
+        DEBIT_COL = 2
+        CREDIT_COL = 3
+        DEBTOR_COL = 4
+        CREDITOR_COL = 5
+
+
+        # Getting the formats.
+        formats = PaySheetReport.get_formats(workbook)
+        white_bold_record = formats['white_bold_record']
+        white_bold_right_currency = formats['white_bold_right_currency']
+        gray_bold_right_currency = formats['gray_bold_right_currency']
+        yellow_total_right = formats['yellow_total_right']
+
+        grouped_by_account_policies_set = paysheet_detail_set.values('account__grouping_code__id','account__grouping_code__account_name')\
+            .annotate(Count('account__grouping_code__id'), Count('account__grouping_code__account_name'), total_credit=Sum('credit'), total_debit=Sum('debit'))
+
+        row_counter = START_ROW
+        counter = 1
+
+        # To keep track of the accumulates.
+        accumulated_credit = 0
+        accumulated_debit = 0
+        accumulated_debtor = 0
+        accumulated_creditor = 0
+
+
+        for account_record in grouped_by_account_policies_set:
+
+            name = account_record['account__grouping_code__account_name']
+            total_credit = account_record['total_credit']
+            total_debit = account_record['total_debit']
+
+            total_debtor = total_credit - total_debit
+            total_creditor = total_debit - total_credit
+
+            # Short if. If the value is negative, then turn it into 0.
+            total_debtor = 0 if total_debtor <= 0 else total_debtor
+            total_creditor = 0 if total_creditor <= 0 else total_creditor
+
+            worksheet.write(row_counter, COUNTER_COL, counter, white_bold_record)
+            worksheet.write(row_counter, ACCOUNT_COL, name, white_bold_record)
+            worksheet.write(row_counter, DEBIT_COL, total_debit, white_bold_right_currency)
+            worksheet.write(row_counter, CREDIT_COL, total_credit, white_bold_right_currency)
+            worksheet.write(row_counter, DEBTOR_COL, total_debtor, gray_bold_right_currency)
+            worksheet.write(row_counter, CREDITOR_COL, total_creditor, gray_bold_right_currency)
+
+            # Adding to the accumulates.
+            accumulated_credit += total_credit
+            accumulated_debit += total_debit
+            accumulated_debtor += total_debtor
+            accumulated_creditor += total_creditor
+
+            row_counter += 1
+            counter += 1
+
+        # Adding the accumulated values to the worksheet.
+
+        worksheet.write(row_counter+1, ACCOUNT_COL, "Total", yellow_total_right)
+        worksheet.write(row_counter+1, DEBIT_COL, accumulated_debit, white_bold_right_currency)
+        worksheet.write(row_counter+1, CREDIT_COL, accumulated_credit, white_bold_right_currency)
+        worksheet.write(row_counter+1, DEBTOR_COL, accumulated_debtor, white_bold_right_currency)
+        worksheet.write(row_counter+1, CREDITOR_COL, accumulated_creditor, white_bold_right_currency)
+
+
+
+
---
+++ Accounting/search_engines/paysheet_engine.py
@@ -0,0 +1,220 @@
+# coding=utf-8
+
+from django.db.models.aggregates import Count, Sum
+from django.db.models.query_utils import Q
+
+from Accounting.models import AccountingPolicyDetail, AccountingPolicy
+
+
+class PolicySearchEngine():
+
+
+    def __init__(self,
+                 lower_fiscal_period_year=None,
+                 upper_fiscal_period_year=None,
+                 lower_fiscal_period_month=None,
+                 upper_fiscal_period_month=None,
+                 type_policy_array=None,
+                 lower_folio=None,
+                 upper_folio=None,
+                 lower_registry_date=None,
+                 upper_registry_date=None,
+                 description=None,
+                 lower_account_number=None,
+                 upper_account_number=None,
+                 lower_debit=None,
+                 upper_debit=None,
+                 lower_credit=None,
+                 upper_credit=None,
+                 reference=None,
+                 only_with_transactions=None,
+                 account_array = None,
+                 internal_company=None
+                 ):
+
+        self.lower_fiscal_period_year = lower_fiscal_period_year
+        self.upper_fiscal_period_year = upper_fiscal_period_year
+
+        self.lower_fiscal_period_month = lower_fiscal_period_month
+        self.upper_fiscal_period_month = upper_fiscal_period_month
+
+        self.type_policy_array = type_policy_array
+
+        self.lower_folio = lower_folio
+        self.upper_folio = upper_folio
+
+        self.lower_registry_date = lower_registry_date
+        self.upper_registry_date = upper_registry_date
+
+        self.description = description
+
+        self.lower_account_number = lower_account_number
+        self.upper_account_number = upper_account_number
+
+        self.lower_debit = lower_debit
+        self.upper_debit = upper_debit
+
+        self.lower_credit = lower_credit
+        self.upper_credit = upper_credit
+
+        self.reference = reference
+
+        self.internal_company = internal_company
+
+        self.only_with_transactions = only_with_transactions
+
+        self.account_array = account_array
+
+    def search_policies(self):
+        query = Q()
+
+        if self.lower_fiscal_period_year is not None:
+            query &= Q(fiscal_period__accounting_year__gte=self.lower_fiscal_period_year)
+
+
+        if self.upper_fiscal_period_year is not None:
+            query &= Q(fiscal_period__accounting_year__lte=self.upper_fiscal_period_year)
+
+        if self.lower_fiscal_period_month is not None:
+            query &= Q(fiscal_period__account_period__gte=self.lower_fiscal_period_month)
+
+        if self.upper_fiscal_period_month is not None:
+            query &= Q(fiscal_period__account_period__lte=self.upper_fiscal_period_month)
+
+        if self.type_policy_array is not None:
+            query &= Q(type_policy__id__in=self.type_policy_array)
+
+        if self.lower_folio is not None:
+            query &= Q(folio__gte=self.lower_folio)
+
+        if self.upper_folio is not None:
+            query &= Q(folio__lte=self.upper_folio)
+
+        if self.lower_registry_date is not None:
+            query &= Q(registry_date__gte=self.lower_registry_date)
+
+
+        if self.upper_registry_date is not None:
+            query &= Q(registry_date__lte=self.upper_registry_date)
+
+        if self.description is not None:
+            query &= Q(description__icontains=self.description)
+
+        if self.lower_account_number is not None and self.account_array is None:
+
+            policy_detail_array = []
+            accounting_policy_detail_set = AccountingPolicyDetail.objects\
+                .filter(Q(account__number__gte=self.lower_account_number))\
+                .values('accounting_policy__id').annotate(Count('accounting_policy__id'))
+
+            for record in accounting_policy_detail_set:
+                policy_detail_array.append(record['accounting_policy__id'])
+
+            query &= Q(id__in=policy_detail_array)
+
+        if self.upper_account_number is not None and self.account_array is None:
+            policy_detail_array = []
+            print self.upper_account_number
+            accounting_policy_detail_set = AccountingPolicyDetail.objects \
+                .filter(Q(account__number__lte=self.upper_account_number)) \
+                .values('accounting_policy__id').annotate(Count('accounting_policy__id'))
+
+            for record in accounting_policy_detail_set:
+                policy_detail_array.append(record['accounting_policy__id'])
+
+            query &= Q(id__in=policy_detail_array)
+
+
+        if self.lower_credit is not None:
+            policy_detail_array = []
+            accounting_policy_detail_set = AccountingPolicyDetail.objects \
+                .filter(Q(credit__gte=self.lower_credit)) \
+                .values('accounting_policy__id').annotate(Count('accounting_policy__id'))
+
+            for record in accounting_policy_detail_set:
+                policy_detail_array.append(record['accounting_policy__id'])
+
+            query &= Q(id__in=policy_detail_array)
+
+        if self.upper_credit is not None:
+            policy_detail_array = []
+            accounting_policy_detail_set = AccountingPolicyDetail.objects \
+                .filter(Q(credit__lte=self.upper_credit)) \
+                .values('accounting_policy__id').annotate(Count('accounting_policy__id'))
+
+            for record in accounting_policy_detail_set:
+                policy_detail_array.append(record['accounting_policy__id'])
+
+            query &= Q(id__in=policy_detail_array)
+
+
+        if self.reference is not None:
+            query &= Q(reference__icontains=self.reference)
+
+
+        if self.internal_company is not None:
+            query &= Q(internal_company__id=self.internal_company)
+
+
+        if self.account_array is not None:
+
+            policy_detail_array = []
+            accounting_policy_detail_set = AccountingPolicyDetail.objects \
+                .filter(Q(account__number__in=self.account_array)) \
+                .values('accounting_policy__id').annotate(Count('accounting_policy__id'))
+
+            for record in accounting_policy_detail_set:
+                policy_detail_array.append(record['accounting_policy__id'])
+
+            query &= Q(id__in=policy_detail_array)
+
+
+
+
+        results = AccountingPolicy.objects.filter(query)
+
+
+        if self.only_with_transactions is not None:
+            if bool(int(self.only_with_transactions)) == True:
+                exclude_array = []
+                for record in results:
+                    accounting_policy_detail_set = AccountingPolicyDetail.objects.filter(Q(accounting_policy__id=record.id))
+                    if len(accounting_policy_detail_set) <= 0:
+                        exclude_array.append(record.id)
+
+                results = results.exclude(Q(id__in=exclude_array))
+
+
+        # Excluding the policies that overpass the debit and credit limits.
+        exclude_array = []
+        for policy in results:
+            policy_details = AccountingPolicyDetail.objects.filter(accounting_policy__id=policy.id).values('accounting_policy__id')\
+                .annotate(Count('accounting_policy__id'), total_debit=Sum('debit'), total_credit=Sum('credit'))[0]
+
+
+            if self.lower_debit is not None:
+                if policy_details['total_debit'] < float(self.lower_debit):
+                    exclude_array.append(policy.id)
+
+            if self.upper_debit is not None:
+                if policy_details['total_debit'] > float(self.upper_debit):
+                    print "Excluding: " + str(policy.id)
+                    exclude_array.append(policy.id)
+
+            if self.lower_credit is not None:
+                if policy_details['total_credit'] < float(self.lower_credit):
+                    exclude_array.append(policy.id)
+
+            if self.upper_credit is not None:
+                if policy_details['total_credit'] > float(self.upper_credit):
+                    exclude_array.append(policy.id)
+
+
+        results= results.exclude(Q(id__in=exclude_array))
+
+        return results
+
+
+
+
+
